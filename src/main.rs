5b21ce3b366b0f8a14fb8b282eff49e9c1477eff
Ryan P. Brewster <RyanPBrewster@gmail.com>
2021-02-27 16:30:04 -0500

    allow descending into the tree

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,15 @@ use termion::style;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::from_args();
-    let data: Value = {
+    let root: Value = {
         let fin = std::fs::File::open(args.input)?;
         let reader = BufReader::new(fin);
         serde_json::from_reader(reader)?
     };
+    let mut data = &root;
+
     let mut path = vec![0];
-    let fields = get_fields(&data);
+    let mut fields = get_fields(data);
 
     let stdin = std::io::stdin();
     let mut stdout = HideCursor::from(std::io::stdout().into_raw_mode()?);
@@ -41,6 +43,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                             *idx -= 1;
                         }
                     }
+                    Key::Char('l') => {
+                        data = descend(data, *path.last().unwrap());
+                        fields = get_fields(data);
+                        path.push(0);
+                    }
                     _ => continue,
                 };
             }
@@ -60,6 +67,19 @@ fn get_fields(v: &Value) -> Vec<String> {
             vec![]
         }
         Value::Object(ref obj) => obj.keys().cloned().collect(),
+    }
+}
+
+fn descend(v: &Value, idx: usize) -> &Value {
+    match v {
+        Value::Array(_) | Value::Null | Value::Bool(_) | Value::Number(_) | Value::String(_) => v,
+        Value::Object(ref obj) => {
+            let key = match obj.keys().nth(idx) {
+                None => return v,
+                Some(key) => key,
+            };
+            obj.get(key).unwrap_or(v)
+        }
     }
 }
 
