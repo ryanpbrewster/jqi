5c14237a64bf922ccdb64a35f5c228d8f66d639f
Ryan P. Brewster <RyanPBrewster@gmail.com>
2021-02-27 15:47:45 -0500

    kinda working

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,49 @@
+use serde_json::Value;
 use std::io::BufReader;
+use std::io::Write;
 use std::path::PathBuf;
 use structopt::StructOpt;
+use termion::event::{Event, Key};
+use termion::input::{MouseTerminal, TermRead};
+use termion::{cursor::Goto, raw::IntoRawMode};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::from_args();
-    let data: serde_json::Value = {
+    let data: Value = {
         let fin = std::fs::File::open(args.input)?;
         let reader = BufReader::new(fin);
         serde_json::from_reader(reader)?
     };
-    println!("{:?}", data);
+    let stdin = std::io::stdin();
+    let mut stdout = MouseTerminal::from(std::io::stdout().into_raw_mode()?);
+
+    write!(stdout, "{}", termion::clear::All)?;
+    stdout.flush()?;
+    for evt in stdin.events() {
+        match evt? {
+            Event::Key(key) => {
+                match key {
+                    Key::Esc => break,
+                    _ => {}
+                };
+            }
+            _ => {}
+        }
+        write!(stdout, "{}{}", termion::clear::All, Goto(1, 1))?;
+        let keys: Vec<String> = match data {
+            Value::Array(_)
+            | Value::Null
+            | Value::Bool(_)
+            | Value::Number(_)
+            | Value::String(_) => vec![],
+            Value::Object(ref obj) => obj.keys().cloned().collect(),
+        };
+        for (i, key) in keys.iter().enumerate() {
+            write!(stdout, "{}{}\n", Goto(1, 2 + i as u16), key)?;
+        }
+        stdout.flush()?;
+    }
+
     Ok(())
 }
 
