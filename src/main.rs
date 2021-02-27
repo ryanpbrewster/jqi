7cc4d942f5bab28e0c96341e651dfb9c2226f0ab
Ryan P. Brewster <RyanPBrewster@gmail.com>
2021-02-27 15:33:27 -0500

    read from a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::BufReader;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::from_args();
+    let data: serde_json::Value = {
+        let fin = std::fs::File::open(args.input)?;
+        let reader = BufReader::new(fin);
+        serde_json::from_reader(reader)?
+    };
+    println!("{:?}", data);
+    Ok(())
+}
+
+#[derive(StructOpt)]
+struct Args {
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
 }
